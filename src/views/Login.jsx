9de2fde8f53b4b49d8fadb6270eb2335851d174d
thangs2024 <slmthang2024@gmail.com>
2024-07-19 16:28:17 -0400

    testing recharts

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,5 +1,5 @@
 
-import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer, Cell } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer, Cell, PieChart, Pie } from 'recharts';
 import './Login.scss';
 const data = [{name: 'Budget', value: 3000}, {name: 'Expenses', value: 2540}];
 
@@ -22,6 +22,40 @@ const renderBarChart = (
   </BarChart>
 );
 
+const data2 = [
+  { name: 'Housing', value: 500 },
+  { name: 'Transportation', value: 540 },
+  { name: 'Insurance', value: 800 },
+  { name: 'Personal', value: 200 },
+];
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const RADIAN = Math.PI / 180;
+
+function renderPieChartLabel(e) {
+  return e.name;
+}
+
+const renderPieChart = (
+  <PieChart>
+    <Pie
+      data={data2}
+      cx="50%"
+      cy="50%"
+      labelLine={false}
+      label={renderPieChartLabel}
+      outerRadius={80}
+      fill="#8884d8"
+      dataKey="value"
+    >
+      {data.map((entry, index) => (
+        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+      ))}
+    </Pie>
+  </PieChart>
+)
+
 
 
 
@@ -29,8 +63,11 @@ export default function Login() {
 
     return (
       <div id='charts'>
-        <ResponsiveContainer id='chart1' width="90%" height={300}>
+        <ResponsiveContainer id='chart1' width="85%" height={275}>
           {renderBarChart}
+        </ResponsiveContainer>
+        <ResponsiveContainer id='chart2' width="85%" height={275}>
+          {renderPieChart}
         </ResponsiveContainer>
       </div>
 
